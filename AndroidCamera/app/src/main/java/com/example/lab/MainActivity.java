d4c0c02ed7333eba99c144b0800c43794ff53e44
zhouweiming <373518155@qq.com>
2019-09-27 15:09:19 +0800

    完善了參數獲取

--- AndroidCamera/app/src/main/java/com/example/lab/MainActivity.java
+++ AndroidCamera/app/src/main/java/com/example/lab/MainActivity.java
@@ -7,6 +7,8 @@ import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.widget.Button;
 
+import com.lxj.xpopup.XPopup;
+import com.lxj.xpopup.interfaces.OnSelectListener;
 import com.yanzhenjie.permission.Permission;
 
 import java.util.ArrayList;
@@ -37,6 +39,12 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
 
         findViewById(R.id.btn_capture).setOnClickListener(this);
         findViewById(R.id.btn_get_preview_resolution).setOnClickListener(this);
+        findViewById(R.id.btn_get_picture_resolution).setOnClickListener(this);
+        findViewById(R.id.btn_get_video_resolution).setOnClickListener(this);
+        findViewById(R.id.btn_get_focus_mode).setOnClickListener(this);
+        findViewById(R.id.btn_get_picture_format).setOnClickListener(this);
+        findViewById(R.id.btn_get_preview_format).setOnClickListener(this);
+        findViewById(R.id.btn_get_preview_fps_range).setOnClickListener(this);
     }
 
     @Override
@@ -50,23 +58,132 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
             Intent intent = new Intent(this, CameraActivity.class);
             startActivity(intent);
         } else if (id == R.id.btn_get_preview_resolution) {
-            getPreviewResolution(ParameterType.PREVIEW_SIZE);
+            String[] result = getParameters(ParameterType.PREVIEW_SIZE);
+            showResultPopup("预览分辨率", result, -1);
+        } else if (id == R.id.btn_get_picture_resolution) {
+            String[] result = getParameters(ParameterType.PICTURE_SIZE);
+            showResultPopup("圖片分辨率", result, -1);
+        } else if (id == R.id.btn_get_video_resolution) {
+            String[] result = getParameters(ParameterType.VIDEO_SIZE);
+            showResultPopup("視頻分辨率", result, -1);
+        } else if (id == R.id.btn_get_focus_mode) {
+            String[] result = getParameters(ParameterType.FOCUS_MODE);
+            showResultPopup("對焦模式", result, -1);
+        } else if (id == R.id.btn_get_picture_format) {
+            String[] result = getParameters(ParameterType.PICTURE_FORMAT);
+            showResultPopup("圖片格式", result, -1);
+        } else if (id == R.id.btn_get_preview_format) {
+            String[] result = getParameters(ParameterType.PREVIEW_FORMAT);
+            showResultPopup("預覽格式", result, -1);
+        } else if (id == R.id.btn_get_preview_fps_range) {
+            String[] result = getParameters(ParameterType.PREVIEW_FPS_RANGE);
+            showResultPopup("預覽幀率", result, -1);
         }
     }
 
-    private List<String> getPreviewResolution(ParameterType parameterType) {
+    private String[] getParameters(ParameterType parameterType) {
         Camera camera = Camera.open();
         Camera.Parameters parameters = camera.getParameters();
 
         List<String> result = new ArrayList<>();
+        if (parameterType == ParameterType.PREVIEW_SIZE || parameterType == ParameterType.PICTURE_SIZE ||
+                parameterType == ParameterType.VIDEO_SIZE) {
+            List<Camera.Size> sizeList = null;
+            if (parameterType == ParameterType.PREVIEW_SIZE) {
+                sizeList = parameters.getSupportedPreviewSizes();
+            } else if (parameterType == ParameterType.PICTURE_SIZE) {
+                sizeList = parameters.getSupportedPictureSizes();
+            } else if (parameterType == ParameterType.VIDEO_SIZE) {
+                sizeList = parameters.getSupportedVideoSizes();
+            }
 
-        List<Camera.Size> sizeList = parameters.getSupportedPreviewSizes();
-        for (Camera.Size size : sizeList) {
-            SLog.info("SIZE[%d X %d]", size.width, size.height);
+            for (Camera.Size size : sizeList) {
+                SLog.info("SIZE[%d X %d]", size.width, size.height);
+                String item = String.format("%d X %d", size.width, size.height);
+                result.add(item);
+            }
+        } else if (parameterType == ParameterType.FOCUS_MODE) {
+            result = parameters.getSupportedFocusModes();
+        } else if (parameterType == ParameterType.PICTURE_FORMAT) {
+            List<Integer> formatList = parameters.getSupportedPictureFormats();
+            for (int format : formatList) {
+                String formatDesc = translateImageFormat(format);
+                result.add(formatDesc);
+            }
+        } else if (parameterType == ParameterType.PREVIEW_FORMAT) {
+            List<Integer> formatList = parameters.getSupportedPreviewFormats();
+            for (int format : formatList) {
+                String formatDesc = translatePreviewFormat(format);
+                result.add(formatDesc);
+            }
+        } else if (parameterType == ParameterType.PREVIEW_FPS_RANGE) {
+            List<int[]> fpsRangeList = parameters.getSupportedPreviewFpsRange();
+            for (int[] fpsRange : fpsRangeList) {
+                String item = String.format("%.2f(min) - %.2f(max)", ((float) fpsRange[0]) / 1000, ((float) fpsRange[1]) / 1000);
+                result.add(item);
+            }
         }
-        camera.release();
+        
 
-        return result;
+        camera.release();
+        return stringListToArray(result);
+    }
+
+    /**
+     * 显示结果弹窗
+     * @param title 标题
+     * @param data 数据
+     * @param checkedPosition 选中哪一项，从0开始，-1表示不选中
+     */
+    private void showResultPopup(String title, String[] data, int checkedPosition) {
+        new XPopup.Builder(this)
+                .asCenterList(title, data,
+                        null, 1,
+                        new OnSelectListener() {
+                            @Override
+                            public void onSelect(int position, String text) {
+                                SLog.info("");
+                            }
+                        })
+                .show();
+    }
+
+    private String[] stringListToArray(List<String> stringList) {
+        if (stringList == null || stringList.size() < 1) {
+            return null;
+        }
+        String[] stringArray = new String[stringList.size()];
+
+        int index = 0;
+        for (String str : stringList) {
+
+            stringArray[index] = str;
+            index++;
+        }
+
+        return stringArray;
+    }
+
+    private String translateImageFormat(int format) {
+        SLog.info("format[%d]", format);
+        switch (format) {
+            case 0x100:
+                return "JPEG";
+            default:
+                return "UNKNOWN";
+        }
+    }
+
+    private String translatePreviewFormat(int format) {
+        SLog.info("format[%d]", format);
+        switch (format) {
+            case 0x11:
+                return "NV21";
+            case 0x32315659:
+                return "YV12";
+            default:
+                return "UNKNOWN";
+        }
     }
 }
 
--- AndroidCamera/app/src/main/java/com/example/lab/ParameterType.java
+++ AndroidCamera/app/src/main/java/com/example/lab/ParameterType.java
@@ -8,5 +8,28 @@ public enum ParameterType {
      * 预览分辨率
      */
     PREVIEW_SIZE,
-
+    /**
+     * 圖片大小
+     */
+    PICTURE_SIZE,
+    /**
+     * 視頻大小
+     */
+    VIDEO_SIZE,
+    /**
+     * 對焦模式
+     */
+    FOCUS_MODE,
+    /**
+     * 圖片格式
+     */
+    PICTURE_FORMAT,
+    /**
+     * 預覽格式
+     */
+    PREVIEW_FORMAT,
+    /**
+     * 预览帧率范围
+     */
+    PREVIEW_FPS_RANGE,
 }

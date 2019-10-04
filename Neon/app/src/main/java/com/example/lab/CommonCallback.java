18982f959932bfabe978a7446ed4d08dba55a29d
zhouweiming <373518155@qq.com>
2019-10-04 17:59:43 +0800

    Neon实验

---
+++ Neon/app/src/main/java/com/example/lab/CommonCallback.java
@@ -0,0 +1,25 @@
+package com.example.lab;
+
+import android.support.annotation.Nullable;
+
+/**
+ * 通用回调接口
+ * Created by zwm
+ * 2018/9/23 11:02
+ */
+public interface CommonCallback {
+    /**
+     * 成功时的回调
+     * @param data
+     * @return
+     */
+    String onSuccess(@Nullable String data);
+
+
+    /**
+     * 失败时的回调
+     * @param data
+     * @return
+     */
+    String onFailure(@Nullable String data);
+}
---
+++ Neon/app/src/main/java/com/example/lab/MainActivity.java
@@ -0,0 +1,22 @@
+package com.example.lab;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+
+public class MainActivity extends AppCompatActivity implements View.OnClickListener {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        SLog.info("onCreate");
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        PermissionUtil.requestStoragePermission(this);
+    }
+
+    @Override
+    public void onClick(View view) {
+        int id = view.getId();
+    }
+}

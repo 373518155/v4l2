c2c48c8e74ed7521aa4ffb50f052e90460a7e049
zhouweiming <373518155@qq.com>
2019-11-04 08:34:28 +0800

    改为用配置文件方式

--- trident-server-java/src/com/example/Main.java
+++ trident-server-java/src/com/example/Main.java
@@ -10,10 +10,6 @@ import java.util.concurrent.ThreadPoolExecutor;
 import java.util.concurrent.TimeUnit;
 
 public class Main {
-    public static final String IP = "127.0.0.1";
-    public static final int PORT = 6677;
-
-
     // 線程等待隊列大小
     private static final int THREAD_QUEUE_SIZE = 1024;
     // 線程池大小
@@ -22,18 +18,29 @@ public class Main {
     private static ExecutorService executorService;
 
     public static void main(String[] args) {
+
         try {
+
+            File cfgFile = new File("config.txt");
+            byte[] cfgContent = Files.readAllBytes(cfgFile.toPath());
+            String config = new String(cfgContent);
+            String[] configArr = config.split(":");
+            String ip = configArr[0];
+            int port = Integer.valueOf(configArr[1]);
+
+            SLog.info("server address[%s:%d]", ip, port);
+
             // 初始化線程池
             executorService =
                     new ThreadPoolExecutor(THREAD_POOL_SIZE, THREAD_POOL_SIZE, 0L, TimeUnit.MILLISECONDS,
                             new LinkedBlockingQueue<Runnable>(THREAD_QUEUE_SIZE));
 
-            ServerSocket ss = new ServerSocket(PORT);
+            ServerSocket ss = new ServerSocket(port);
             System.out.println("启动服务器....");
 
             while (true) {
                 Socket s = ss.accept();
-                executorService.execute(new Worker(s, IP, PORT));
+                executorService.execute(new Worker(s, ip, port));
             }
         } catch (IOException e) {
             e.printStackTrace();
